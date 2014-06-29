151978b725fdfce8a07548eee3a798dc6d823c5a
weitsai <begining1003@gmail.com>
2014-06-30 03:16:55 +0800

    新增 class_name 變數，供使用者輸入 class name

--- jsonToGsonClass.php
+++ jsonToGsonClass.php
@@ -1,11 +1,12 @@
 <?php
+$class_name = 'weitsai_test';
 $show_set_function = false;
 $show_get_function = true;
 
 $json = '{"from_uid":"adfsdfe3rwersdf343","color":"#000","from_username": "weitsai","message": "android send"}';
 $keys = array_keys(json_decode($json, true));
 
-echo 'public class test {' . PHP_EOL;
+echo "public class $class_name {" . PHP_EOL;
 
 // Declare variables
 array_walk($keys, function($val, $key) {
