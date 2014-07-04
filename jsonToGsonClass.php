6f794807ddcf5e560d9716f3c6aba750c34d0456
weitsai <begining1003@gmail.com>
2014-07-04 10:54:07 +0800

    新增是否要 parse 某欄位的功能

--- jsonToGsonClass.php
+++ jsonToGsonClass.php
@@ -15,17 +15,21 @@ echo "public class $class_name {" . PHP_EOL;
 
 // Declare variables
 array_walk($keys, function($val, $key) {
-    echo "\tprivate String {$val};\n";
+    $isShow = isset($_GET[$val]) ? $_GET[$val] : false;
+    if ($isShow != false) {
+        echo "\tprivate String {$val};\n";
+    }
 });
 
 // Declare set/get function
 // 變數字首大寫
 array_walk($keys, function($val, $key) use ($show_set_function, $show_get_function){
-    if ($show_set_function) {
+    $isShow = isset($_GET[$val]) ? $_GET[$val] : false;
+    if ($show_set_function AND $isShow != false) {
         echo "\n\tpublic void set" . capitalize($val) . "(String $val) {\n\t}\n";
     }
 
-    if ($show_get_function) {
+    if ($show_get_function AND $isShow != false) {
         echo "\n\tpublic String get" . capitalize($val) . "() {\n\t\treturn this.{$val};\n\t}\n";
     }
 });
